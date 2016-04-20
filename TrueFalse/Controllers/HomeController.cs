5491220c053ffa5af0494fa166f261b1fe73b40a
PDDaniel <daniel@proper-dog.com>
2016-04-20 12:45:46 +0200

    Initial Commit

---
+++ TrueFalse/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrueFalse.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TrueFalseSubmit(bool test)
+        {
+            TempData["Test"] = test.ToString();
+            return RedirectToAction("Index");
+        }
+    }
+}
